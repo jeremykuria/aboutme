cbe625ee6d94e4ce0b12c73e5fc22707bd4a4091
Jeremiah kuria <jkaranja@meliora.tech>
2021-10-19 06:11:19 +0300

    made menus responsive

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,12 @@ $(document).ready(function() {
             $('.navbar').removeClass('sticky')
 
         }
-    })
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function() {
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+
 
 })
